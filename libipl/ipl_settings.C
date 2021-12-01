faea57862a4fdea544a7db35be2f07fe46b04016
Ramesh Iyyar <rameshi1@in.ibm.com>
2021-12-01 04:59:02 -0600

    libipl: Switched to ipl_settings for the ipl mode
    Signed-off-by: Ramesh Iyyar <rameshi1@in.ibm.com>

--- libipl/ipl_settings.C
+++ libipl/ipl_settings.C
@@ -19,3 +19,31 @@ ipl_settings::ipl_settings() :
     _log_func_priv_data(NULL),
     _error_callback_fn(NULL)
 {}
+
+void ipl_set_mode(enum ipl_mode mode)
+{
+    switch (mode) {
+        case IPL_AUTOBOOT:
+            ipl_log(IPL_INFO, "IPL mode set to AUTOBOOT\n");
+            break;
+
+        case IPL_HOSTBOOT:
+            ipl_log(IPL_INFO, "IPL mode set to HOSTBOOT\n");
+            break;
+
+        case IPL_CRONUS:
+            ipl_log(IPL_INFO, "IPL mode set to CRONUS\n");
+            break;
+
+        default:
+            ipl_log(IPL_ERROR, "Invalid IPL mode\n");
+            assert(0);
+    }
+
+    g_ipl_settings._mode = mode;
+}
+
+enum ipl_mode ipl_mode(void)
+{
+    return g_ipl_settings._mode;
+}
